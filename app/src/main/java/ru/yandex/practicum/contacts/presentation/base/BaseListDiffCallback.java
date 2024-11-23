246873ce1f38d9ff0e9653fbbd6cccb1f15ef912
LightQWE <rpappra@gmail.com>
2024-11-23 13:34:07 +0500

    домашка

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -1,21 +1,22 @@
 package ru.yandex.practicum.contacts.presentation.base;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.DiffUtil;
 
 public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T>{
 
-    public boolean theSameAs(T other){
-        return this.hashCode() == other.hashCode();
-
-    }
-    public boolean areItemsTheSame( T oldItem,  T newItem) {
+    @Override
+    public boolean areItemsTheSame(@NonNull T oldItem,@NonNull  T newItem) {
         return oldItem.theSameAs(newItem);
     }
 
-
-    public boolean areContentsTheSame( T oldItem,  T newItem) {
+    @Override
+    public boolean areContentsTheSame(@NonNull T oldItem,@NonNull  T newItem) {
         return oldItem.equals(newItem);
     }
-    public Object getChangePayload( T oldItem,  T newItem) {
+    @Nullable
+    @Override
+    public Object getChangePayload(@NonNull T oldItem, @NonNull  T newItem) {
         return newItem;
     }
 }
